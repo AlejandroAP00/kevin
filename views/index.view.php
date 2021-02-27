2612f5c8babf26fc900d32dd052a75d921e29200
KENDSAO <79384374+KENDSAO@users.noreply.github.com>
2021-02-27 18:04:17 -0500

    Pagina
    xd

--- views/index.view.php
+++ views/index.view.php
@@ -1,62 +1,223 @@
-<?php require_once('admin/config.php'); ?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" class="no-scroll"> <!-- no-scroll -->
 	<head>
 		<meta charset="UTF-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<title>Title site</title>
+   
+		<title> </title>
 
 		<meta property="og:url"                content="<?php echo $URL; ?>" />
 		<meta property="og:type"               content="website" />
-		<meta property="og:title"              content="Title site" />
-		<meta property="og:description"        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. " />
-		<meta property="og:image"              content="<?php echo $URL; ?>/img/sharing/example.png" />
+		<meta property="og:title"              content=" " />
+		<meta property="og:description"        content=" " />
+		<meta property="og:image"              content=" " />
+
 
 		<?php echo $favicon; ?>
-		<?php echo $normalize_css; ?>
 		<?php echo $boostrap_v4_css ?>
 
-		<!-- Font awesome -->
-		<?php echo $font_awesome_css; ?>
+
 		<!-- CSS own files -->
 		<?php echo $own_file_css; ?>
-		
-		<?php echo $google_analitycs; ?>
+
+<link rel="stylesheet" type="text/css" href="scss/kevin.scss">
+<link href = "https://fonts.googleapis.com/css2? family = Gilda + Display & display = "swap" rel = "hoja de estilo">
+<link href = "https://fonts.googleapis.com/css2? family = Open + Sans: wght @ 300 & display = intercambiar "rel =" hoja de estilo ">
 	</head>
 	<body>
 
-		<?php require("views/navbar.view.php"); ?>
+			<?php require("views/navbar.view.php"); ?>
 
-
-		<section class="kevin">
-			<div class="container">
-				<div class="row">
-					
-					<div class="abuelo ">
-						
-					</div>
-
-				</div>
+	<div class="container-fluid100 ">
+		<div class="abuelo"></div>
+			<div class="abuelo dos">
+				<div class="bolitas 1">
 			</div>
-		</section>	
+		</div>	
+	</div>	
 
-		<div class="abuelo dos">
-			<div class="bolitas 1"></div>
-			<div class="bolita2"></div>
-			<div class="bolita3"></div>
-			<h1>laminex</h1>
-		</div>		
+	<div class="container101">
+  	</div>
 
-		<figure>
-			<img src="" alt="">
-		</figure>
-		<?php require("views/footer.view.php"); ?>	
-		
+	<div class="container102">
+		<img src="<?=RUTA?>/img/c6.png" alt="" >
+  	</div>
+  	<div class="container103">
+		<img src="<?=RUTA?>/img/c10.png" alt="" >
+  	</div>
+
+	<div class="container104">
+			<img src="<?=RUTA?>/img/mapa.png" alt="" >
+
+			<p><span style="color:hsla(30, 97%, 53%, 1)">T.</span>9999 234567<p>
+			<p><span style="color:hsla(30, 97%, 53%, 1)">D.</span>Calle x #000-E x 0 y 0, Colonia<br> Esperanza. Mérida, Yuc. MX</p>
+			<p><span style="color:hsla(30, 97%, 53%, 1)">M.</span>mail@example.com</p>
+
+	 </div>
+
+
+
+	 <div class="container-fluid105 ">
+		<div class="abuelo"></div>
+			<div class="abuelo dos">
+				<div class="bolitas 1">
+			</div>
+		</div>	
+	</div>	
+
+
+	<div class="container-fluid106">
+		<div class="abuelo"></div>
+			<div class="abuelo dos">
+				<div class="bolitas 1">
+			</div>
+		</div>	
+	</div>	
+
+
+
+<section class="cuerpo1">
+	<div class="container"> 
+		<div class="titulo1 offset-1">
+				<h1>SERVICIOS</h1>
+		</div>
+		<div class="titulo2 offset-1">
+				<h1>LAMINEX</h1>
+		</div>
+	</div>
+
+		<div class="container1">
+	  			<div class="row" style="column-count:2 ;">
+		  			<div class="col-1 offset-1">
+		  				<img src="<?=RUTA?>/img/c1.png" alt="" >
+		  			</div>
+		  			<div class="col-3" style="margin-top:20px;">
+		  				<span>Cambio de cubiertas y muros de lámina</span>
+		  			</div>
+		  		</div>
+  		</div>
+
+  		<div class="container2">
+	  			<div class="row" style="column-count:2 ;">
+		  			<div class="col-1 offset-1">
+		  				<img src="<?=RUTA?>/img/c2.png" alt="" >
+		  			</div>
+		  			<div class="col-3" style="margin-top:20px;">
+		  				<span>Mantenimiento de cubiertas metálicas</span>
+		  			</div>
+		  		</div>
+  		</div>
+
+  		<div class="container3">
+	  			<div class="row" style="column-count:2 ;">
+		  			<div class="col-1 offset-1">
+		  				<img src="<?=RUTA?>/img/c3.png" alt="" >
+		  			</div>
+		  			<div class="col-3" style="margin-top:20px;">
+		  				<span>Cambio y/o mantenimiento de canalones, remates, cumbreras.</span>
+		  			</div>
+		  		</div>
+  		</div>
+
+  		<div class="container4">
+	  			<div class="row" style="column-count:2 ;">
+		  			<div class="col-1 offset-1">
+		  				<img src="<?=RUTA?>/img/c4.png" alt="" >
+		  			</div>
+		  			<div class="col-3" style="margin-top:20px;">
+		  				<span>Cambio de láminas acrílicas</span>
+		  			</div>
+		  		</div>
+  		</div>
+
+  		<div class="container5">
+	  			<div class="row" style="column-count:2 ;">
+		  			<div class="col-1 offset-1">
+		  				<img src="<?=RUTA?>/img/c5.png" alt="" >
+		  			</div>
+		  			<div class="col-3" style="margin-top:20px;">
+		  				<span>Pintura e impermeabilización de cubiertas y estructura</span>
+		  			</div>
+		  		</div>
+  		</div>
+
+</section>
+
+<br><br><br>
+
+<section class="cuerpo2">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-4 offset-">
+					<img src="<?=RUTA?>/img/c7.png" alt="" >
+					<p>Trabajamos en todo el<br> sureste mexicano</p>
+				
+			</div>
+			<div class="col-4 offset-">
+					<img src="<?=RUTA?>/img/c8.png" alt="" >
+				<p>Asesoría<br> y atención al cliente</p>
+			</div>
+			<div class="col-4 offset-">
+					<img src="<?=RUTA?>/img/c9.png" alt="" >
+				<p>Mano de obra calificada y<br> con amplia experiencia</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="cuerpo3">
+	<div class="container"> 
+		<div class="titulo1 offset-1">
+				<h1>ACERCA DE</h1>
+		</div>
+		<div class="titulo2 offset-1">
+				<h1>LAMINEX</h1>
+		</div>
+		<div class="col-6 offset-">			
+				<p>LAMINEX, somos una empresa yucateca que nace ante la necesidad en el mercado de un <strong>servicio en el mantenimiento de cubiertas metálicas.</strong><br><br> 
+
+				 Procuramos a nuestros clientes una solución pronta, eficaz y redituable, de sus problemas de cubiertas.<br> <br>  
+
+				El personal de LAMINEX cuenta con una extensa experiencia en los trabajos que brinda, aunque la empresa es un negocio de nueva creación, garantiza los trabajos y servicios ofrecidos. Para ello, nos comprometemos a dar <strong>las mejores soluciones</strong> que se adapten a las necesidades particulares y específicas que tenga, su cubierta de lámina, así como ofrecer la mejor calidad en productos y recursos humanos, teniendo como principales valores; el compromiso, la responsabilidad y la eficacia, la seguridad y la capacidad para afrontar cualquiera que sea la envergadura del trabajo.<br> <br>
+
+				 En LAMINEX podemos trabajar en <strong>cualquier parte del sureste de México</strong>; (Campeche, Yucatán y Quintana Roo). Como empresa, LAMINEX su principal objetivo es, hacerse de una reputación y prestigio íntegro, en el ramo.
+				</p>
+			</div>
+	</div>
+</section>
+ 
+
+<section class="cuerpo4">
+	<div class="container">
+		<div class="titulo1 offset-1">
+				<h1>CONTACTO</h1>
+		</div><br>
+
+			<div class="cuadros1">
+					<form id="form1" name="form1" method="post" action="">
+						<input type="text" name="a" /><br>
+				 		<input type="text" name="b" /><br> 
+						<input type="text" name="b" />
+					</form>
+			</div>
+
+			<div class="cuadros2">
+				<form id="form1" name="form1" method="post" action="">
+				 	<input type="text" name="a" />
+				</form>
+			</div>
+
+			<div class="cuadros3">
+				<form id="form1" name="form1" method="post" action="">
+				 	<input type="submit" name="Submit" value="Enviar" />
+				</form>
+			</div>
+</section>
+
+			<?php require("views/footer.view.php"); ?>
+			
 		<!-- jQuery first, then Tether, then Bootstrap JS. -->
 		<?php echo $jquery_js; ?>
-		<?php echo $tether_js; ?>
 
 
 		<!-- Javascripts own files  -->		
@@ -65,7 +226,15 @@
 
 		<?php echo $bootstrap_v4_js; ?>
 
-		<?php echo $font_awesome_js; ?>
+
+
+
+
+		<script src='https://www.google.com/recaptcha/api.js?hl=es'></script>
+		<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 		
 	</body>
 </html>
